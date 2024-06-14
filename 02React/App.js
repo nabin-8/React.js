f74b1f59ec4850054a9d0b601c72f23dc3a567ff
nabin-8 <nabinacharya740@gmail.com>
2024-06-14 15:56:55 +0545

    reactbuldler

---
+++ 02React/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},[
+    React.createElement(
+        "div",
+        {id:"child"},[
+        React.createElement("h1", {}, "This is Namesta React"),
+        React.createElement("h2", {}, "I am h1 tag")
+    ]
+    ),
+    React.createElement(
+        "div",
+        {id:"child"},[
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h1 tag")
+    ]
+    )]
+);
+
+const heading=React.createElement("h1", {id:"heading"}, "Hello World From React");
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React.js
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Alogrithm - Written in C++
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differencing Bundling - support older browser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and production bundles
